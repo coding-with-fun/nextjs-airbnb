69235aa2cf5e511d53c74c78111b28528b4ddacf
Harrsh Patel <dev.harrsh@gmail.com>
2023-04-05 18:33:35 +0530

    Started with Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 export const metadata = {
   title: "Airbnb",
@@ -19,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
